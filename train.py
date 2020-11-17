390fab5019aa4cef9362d8ed10c7c974b5c0a139
Zachary Minot <43081846+ZMinot@users.noreply.github.com>
2020-11-17 18:42:10 -0500

    Add single imposter training

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ from log import *
 brange = None
 
 
-def run_batch(device, crew, imposter, crew_const=None):
+def run_batch(device, crew, imposter, crew_const=None, imposter_const=None):
     global brange
     player_ix, crew_views, imposter_views = generate_views(
         B, N, P, I, view_chance=VIEW_CHANCE, sabotage_chance=SABOTAGE_CHANCE
@@ -15,76 +15,90 @@ def run_batch(device, crew, imposter, crew_const=None):
     imposter_views = torch.tensor(imposter_views).float().to(device)
 
     # Viewer Stage
-    memory = []  # Result from Viewer(view) of each player
+    memory = []
     if brange is None:  # Only create brange once
         brange = torch.arange(B).to(device)
     for p in range(P):
-        if p < I:  # If player is imposter
+        # Imposter
+        if p < I:
             my_view = imposter_views[:, p, :, :]
-            memory.append(imposter.viewer(my_view)[1])  # Imposter's view pass
-        elif crew_const is not None and p == I:
+            if imposter_const is not None and p != 0:
+                memory.append(imposter_const.viewer(my_view)[1])
+            else:
+                memory.append(imposter.viewer(my_view)[1])
+        # Crew
+        else:
             my_view = crew_views[:, p - I, :, :]
-            memory.append(crew.viewer(my_view)[1])  # Train Crew's view pass
-        elif crew_const is not None:
-            my_view = crew_views[:, p - I, :, :]
-            memory.append(crew_const.viewer(my_view)[1])  # Constant crew view pass
-        else:  # Player is crew
-            my_view = crew_views[:, p - I, :, :]
-            memory.append(crew.viewer(my_view)[1])  # Crew's view pass
+            if crew_const is not None and p != I:
+                memory.append(crew_const.viewer(my_view)[1])
+            else:
+                memory.append(crew.viewer(my_view)[1])
 
     # Communicate Stage
-    messages = torch.zeros((B, P, M)).to(device)  # Communication data
-    for p in range(P):  # Create initial message
-        h_0, _ = memory[p]  # Grab initial memory from player
+    messages = torch.zeros((B, P, M)).to(device)
+    for p in range(P):
+        h_0, _ = memory[p]
         message = None
-        if p < I:  # If player is imposter
-            message = imposter.comm.get_message(h_0)  # Pass imposter initial h_0
-        elif crew_const is not None and p == I:
-            message = crew.comm.get_message(h_0)  # Train crew initial h_0
-        elif crew_const is not None:
-            message = crew_const.comm.get_message(h_0)  # Constant crew initial h_0
-        else:  # Player is crew
-            message = crew.comm.get_message(h_0)  # Pass crew initial h_0
+        # Imposter
+        if p < I:
+            if imposter_const is not None and p != 0:
+                message = imposter_const.comm.get_message(h_0)
+            else:
+                message = imposter.comm.get_message(h_0)
+        # Crew
+        else:
+            if crew_const is not None and p != I:
+                message = crew_const.comm.get_message(h_0)
+            else:
+                message = crew.comm.get_message(h_0)
         messages[brange, player_ix[:, p], :] = message
-    for r in range(ROUNDS):  # Actual communication
-        new_messages = torch.zeros((B, P, M)).to(device)  # Next round comm data
+    for r in range(ROUNDS):
+        new_messages = torch.zeros((B, P, M)).to(device)
         for p in range(P):
-            if p < I:  # If player is imposter
-                message, memory[p] = imposter.comm(messages.view(B, P * M), memory[p])
-                new_messages[brange, player_ix[:, p], :] = message  # Imposter comm data
-            elif crew_const is not None and p == I:
-                message, memory[p] = crew.comm(messages.view(B, P * M), memory[p])
-                new_messages[
-                    brange, player_ix[:, p], :
-                ] = message  # Train Crew comm data
-            elif crew_const is not None:
-                message, memory[p] = crew_const.comm(messages.view(B, P * M), memory[p])
-                new_messages[
-                    brange, player_ix[:, p], :
-                ] = message  # Constant crew comm data
-            else:  # Player is crew
-                message, memory[p] = crew.comm(messages.view(B, P * M), memory[p])
-                new_messages[brange, player_ix[:, p], :] = message  # Crew comm data
+            # Imposter
+            if p < I:
+                if imposter_const is not None and p != 0:
+                    message, memory[p] = imposter_const.comm(
+                        messages.view(B, P * M), memory[p]
+                    )
+                else:
+                    message, memory[p] = imposter.comm(
+                        messages.view(B, P * M), memory[p]
+                    )
+            else:
+                if crew_const is not None and p != I:
+                    message, memory[p] = crew_const.comm(
+                        messages.view(B, P * M), memory[p]
+                    )
+                else:
+                    message, memory[p] = crew.comm(messages.view(B, P * M), memory[p])
+            new_messages[brange, player_ix[:, p], :] = message
         messages = new_messages
 
     # Vote stage
     votes = 0
     for p in range(P):
-        if p < I:  # If player is imposter
-            votes += imposter.vote(memory[p])  # Imposter votes
-        elif crew_const is not None and p == I:
-            votes += crew.vote(memory[p])  # Train crew votes
-        elif crew_const is not None:
-            votes += crew_const.vote(memory[p])  # Constant crew votes
-        else:  # Player is crew
-            votes += crew.vote(memory[p])  # Crew votes
-    votes /= P  # Make values to be [0, 1]
-    imposter_votes = votes[brange[:, None], player_ix[:, :I]]  # Extract imposter votes
-    return torch.mean(imposter_votes.max(1)[0])  # Mean of max votes for imposter
+        # Imposter
+        if p < I:
+            if imposter_const is not None and p != 0:
+                votes += imposter_const.vote(memory[p])
+            else:
+                votes += imposter.vote(memory[p])
+        # Crew
+        else:
+            if crew_const is not None and p != I:
+                votes += crew_const.vote(memory[p])
+            else:
+                votes += crew.vote(memory[p])
+    votes /= P
+    imposter_votes = votes[brange[:, None], player_ix[:, :I]]
+
+    # Crew score is the mean of max votes in each batch for an imposter
+    return torch.mean(imposter_votes.max(1)[0])
 
 
 B = 64
-N = 32
+N = 64
 H = 256
 M = 16
 ROUNDS = 8
@@ -97,6 +111,8 @@ SABOTAGE_CHANCE = 0.5
 TRAIN_CREW_FIRST = False
 TRAIN_SINGLE_CREWMATE = True
 CONST_CREW_COPY_FREQ = 64
+TRAIN_SINGLE_IMPOSTER = True
+CONST_IMPOSTER_COPY_FREQ = 64
 PRINT_FREQ = 64
 
 
@@ -111,6 +127,11 @@ def main():
         crew_const = Agent(device, P, H, M)
         crew_const.requires_grad_(False)
 
+    imposter_const = None
+    if TRAIN_SINGLE_IMPOSTER:
+        imposter_const = Agent(device, P, H, M)
+        imposter_const.requires_grad_(False)
+
     imposter_optim = torch.optim.Adam(imposter.parameters())
     crew_optim = torch.optim.Adam(crew.parameters())
     optimizer = [imposter_optim, crew_optim]
@@ -124,7 +145,13 @@ def main():
         imposter.requires_grad_(not train_crew)
         for b in range(EPOCH_LENGTH):
             optimizer[train_crew].zero_grad()
-            crew_score = run_batch(device, crew, imposter, crew_const=crew_const)
+            crew_score = run_batch(
+                device,
+                crew,
+                imposter,
+                crew_const=crew_const,
+                imposter_const=imposter_const,
+            )
             running_score += crew_score
             loss = crew_score * (-1 if train_crew else 1)
             if b % PRINT_FREQ == PRINT_FREQ - 1:
@@ -140,9 +167,18 @@ def main():
                 and b % CONST_CREW_COPY_FREQ == CONST_CREW_COPY_FREQ - 1
             ):
                 crew_const.copy_state(crew)
+            if (
+                not train_crew
+                and imposter_const is not None
+                and b % CONST_IMPOSTER_COPY_FREQ == CONST_IMPOSTER_COPY_FREQ - 1
+            ):
+                imposter_const.copy_state(imposter)
 
         if train_crew and crew_const is not None:
             crew_const.copy_state(crew)
+
+        if not train_crew and imposter_const is not None:
+            imposter_const.copy_state(imposter)
 
 
 if __name__ == "__main__":
